7c083b8f2c202c77bd1de75c91cf6cac35a8a061
shoner <shonerzamalone@gmail.com>
2016-12-03 09:03:33 +0100

    Add space

--- homeApp/class/Folder.php
+++ homeApp/class/Folder.php
@@ -12,7 +12,7 @@ class Folder {
 			if(is_dir($value)) {
 				$cleans_items['dirs'][$index]['name'] = $value;
 				$cleans_items['dirs'][$index]['date'] = date("d M Y H:i:s.", filemtime($value));
-				$cleans_items['dirs'][$index]['weight'] =disk_total_space($directory.'/'.$value);
+				$cleans_items['dirs'][$index]['weight'] = disk_total_space($directory.'/'.$value);
 			} else {
 				$cleans_items['files'][$index]['name'] = $value;
 				$cleans_items['files'][$index]['date'] = date("d M Y H:i:s.", filemtime($value));
